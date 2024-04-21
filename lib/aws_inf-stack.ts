dae593788e4d098991c330994815f61d08a03b40
Ievgenii Masko <ievgeniimasko@Ievgeniis-MacBook-Pro.local>
2024-04-21 11:31:17 +0200

    init

--- lib/aws_inf-stack.ts
+++ lib/aws_inf-stack.ts
@@ -1,16 +1,58 @@
 import * as cdk from 'aws-cdk-lib';
+import { Port, Vpc } from 'aws-cdk-lib/aws-ec2';
+import {
+  Cluster,
+  ContainerImage,
+  FargateTaskDefinition,
+  LogDriver,
+} from 'aws-cdk-lib/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsInfStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'UrlShortenerVPC', {
+      maxAzs: 2,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsInfQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new Cluster(this, 'UrlShortenerCluster', {
+      vpc,
+    });
+
+    const taskDefinition = new FargateTaskDefinition(
+      this,
+      'UrlShortenerTaskDef',
+      {
+        cpu: 256, // 0.25 vCPU
+        memoryLimitMiB: 512, // 0.5 GB memory
+      },
+    );
+
+    const container = taskDefinition.addContainer('UrlShortenerContainer', {
+      image: ContainerImage.fromRegistry('ievgeniimasko/url_shortener'),
+      logging: LogDriver.awsLogs({ streamPrefix: 'server-container' }),
+    });
+
+    container.addPortMappings({
+      containerPort: 80,
+    });
+
+    const service = new ApplicationLoadBalancedFargateService(
+      this,
+      'UrlShortenerService',
+      {
+        cluster,
+        taskDefinition,
+        desiredCount: 2,
+        publicLoadBalancer: true,
+      },
+    );
+
+    service.service.connections.allowFromAnyIpv4(
+      Port.tcp(80),
+      'Allow inbound HTTP traffic',
+    );
   }
 }
